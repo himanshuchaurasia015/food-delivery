b6c03b6b954643f9d2e400f56db2d167035b8333
Himanshu Chaurasia <himanshuchaurasia015@gmail.com>
2025-01-26 16:46:50 +0530

    script updated

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,4 +4,8 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  build: {
+    outDir: "dist", // Ensure this matches your deployment folder
+  },
+  base: "/",
 });
